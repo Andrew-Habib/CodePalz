2139c6083074c2ce265985be866658aa5abd476a
Andrew Habib <andrew9ca@gmail.com>
2024-08-14 23:56:36 -0400

    Install and Setup Tailwind CSS in React App Environment

--- src/App.js
+++ src/App.js
@@ -3,22 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1
+			class="bg-gradient-to-r from-blue-900 via-purple-600 to-fuchsia-800 text-white hover:text-gray-900 px-4 py-24 m-4 my-10 shadow sm:text-4xl"
+		>
+			My name is Andrew and you're watching + practicing this on codedamn
+		</h1>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
