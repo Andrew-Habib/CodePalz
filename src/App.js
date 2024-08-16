e873c3cae247400857a55b9e3cbb7f441c7dee99
Andrew Habib <andrew9ca@gmail.com>
2024-08-15 21:55:28 -0400

    React Environment Clean up

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <h1
-			class="bg-gradient-to-r from-blue-900 via-purple-600 to-fuchsia-800 text-white hover:text-gray-900 px-4 py-24 m-4 my-10 shadow sm:text-4xl"
-		>
-			My name is Andrew and you're watching + practicing this on codedamn
-		</h1>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <h1></h1>
   );
 }
 
